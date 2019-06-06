49f5d3f9796406c93026121b1c72f6db2a0fde47
d.bulgakov <d.bulgakov@corp.mail.ru>
2019-06-06 13:16:46 +0300

    abort build on error increment version

--- src/main/kotlin/ru/mail/im/motli/App.kt
+++ src/main/kotlin/ru/mail/im/motli/App.kt
@@ -11,37 +11,44 @@ import ru.mail.im.motli.resource.ThemeSet
 import java.io.File
 import java.io.FileReader
 
+private const val ERROR_UNKNOWN_ERROR = 100
+
 fun main(args: Array<String>) {
     if (args.size != 1) {
         System.err.println("Path to config not provided")
         return
     }
 
-    val configFile = File(args[0]).absoluteFile
-    println("Parsing config ${configFile.absolutePath}")
+    try {
+        val configFile = File(args[0]).absoluteFile
+        println("Parsing config ${configFile.absolutePath}")
 
-    val configDto = Gson().fromJson(FileReader(configFile), AppConfigDto::class.java)
-    val config = AppConfig(configFile.parentFile, configDto)
-    config.verify()
+        val configDto = Gson().fromJson(FileReader(configFile), AppConfigDto::class.java)
+        val config = AppConfig(configFile.parentFile, configDto)
+        config.verify()
 
-    val themes = ThemeSet(config)
+        val themes = ThemeSet(config)
 
-    System.out.println("Processing palette")
-    PaletteProcessor(config).fill(themes)
+        System.out.println("Processing palette")
+        PaletteProcessor(config).fill(themes)
 
-    val namingConvention = DefaultNamingConvention()
+        val namingConvention = DefaultNamingConvention()
 
-    System.out.println("Processing color state list resources")
-    ColorStateListProcessor(config, namingConvention).fill(themes)
+        System.out.println("Processing color state list resources")
+        ColorStateListProcessor(config, namingConvention).fill(themes)
 
-    System.out.println("Processing drawable resources")
-    DrawableProcessor(config, namingConvention).fill(themes)
+        System.out.println("Processing drawable resources")
+        DrawableProcessor(config, namingConvention).fill(themes)
 
-    System.out.println("Assembling")
-    val fileSet = ResourceAssembler(config, namingConvention).assemble(themes)
+        System.out.println("Assembling")
+        val fileSet = ResourceAssembler(config, namingConvention).assemble(themes)
 
-    System.out.println("Writing")
-    ResourceWriter(config).write(fileSet)
+        System.out.println("Writing")
+        ResourceWriter(config).write(fileSet)
 
-    System.out.println("Done")
+        System.out.println("Done")
+    } catch (e: Exception) {
+        println("Unknown error $e")
+        System.exit(ERROR_UNKNOWN_ERROR)
+    }
 }
